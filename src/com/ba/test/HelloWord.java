ef134df79aa508f6be2fcf058ca99b8152de33d2
testuser4-at-511582269593 <connect2manin@gmail.com>
2022-05-08 19:11:13 +0530

    git demo files commmited

---
+++ src/com/ba/test/HelloWord.java
@@ -0,0 +1,15 @@
+package com.ba.test;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+	
+	    int a =10;
+	    int b=20;
+	    int c=a+b;
+		
+		System.out.println("Welcome to Hello World");
+
+	}
+
+}
